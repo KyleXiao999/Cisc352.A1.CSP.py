b01a556a14de5854f533fa50d569ad6c66e4a72d
Allen <19xz64@queensu.ca>
2023-02-04 02:25:43 -0500

    Fifth commit - heuristics.py finished - still need to test

--- heuristics.py
+++ heuristics.py
@@ -30,9 +30,44 @@ var_ordering == a function with the following template
 def ord_dh(csp):
     ''' return variables according to the Degree Heuristic '''
     # IMPLEMENT
-    pass
+    unassignedVars = []
+    for vari in csp.variables:
+        if vari.is_assigned() is False:
+            unassignedVars.append(vari)
+
+    maxDegree = -1  #to keep track maximum degree of the variables
+    dhVar = None    #to keep track variable with the maximum degree
+
+    for vari in unassignedVars: #for each unassigned vaiable degree is initialized to 0
+        degree = 0
+
+        for constraint in csp.constraints:    #check if var is in the scope of the constraint
+            if vari in constraint.var_scope:
+                degree += 1
+        if degree > maxDegree:     #check if the degree of var is greater than the current maximum degree
+            maxDegree = degree     #max_degree is updated to the degree of var
+            dhVar = vari            #dh_var is updated to var.
+
+    return dhVar
+
 
 def ord_mrv(csp):
     ''' return variable according to the Minimum Remaining Values heuristic '''
     # IMPLEMENT
-    pass
+    unassignedVars = []  # initialized with an empty list unassigned_vars
+
+    for vari in csp.variables:  #check if the variable is assigned
+        if not vari.is_assigned():
+            unassignedVars.append(vari)  # vari is appended to the unassigned_vars list
+
+    min_remaining = float("inf")  # to keep track of the minimum remaining values of the variables
+    mrv_var = None  # to keep track of the variable with the minimum remaining values
+
+    for vari in unassignedVars:  # compare each unassigned variable with current minimum remaining value min_remaining_val
+        if len(vari.domain) < min_remaining:
+            min_remaining = len(vari.domain)  # min_remaining_val is updated to the length of the domain of var
+            mrv_var = vari  # mrv_var is updated to var
+
+    return mrv_var  # return minimum remaining values
+
+
